25ae447df03e857b6cef291023fa26dad4c2cbc6
Shinton George <shintonseg5@gmail.com>
2024-05-26 01:12:44 +0530

    custom jpql and native query

--- mappings/src/main/java/com/hibernate/mappings/repository/AuthorRepository.java
+++ mappings/src/main/java/com/hibernate/mappings/repository/AuthorRepository.java
@@ -2,8 +2,19 @@ package com.hibernate.mappings.repository;
 
 import com.hibernate.mappings.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+    //JPQL query - on entities and field
+    @Query("SELECT a FROM Author a WHERE a.name LIKE %:keyword%")
+    List<Author> findAuthorsByNameContaining(String keyword);
+
+    // native query for database and for writing complex queries
+    @Query(value = "SELECT * FROM author WHERE name = ?1", nativeQuery = true)
+    List<Author> findAuthorsByName(String name);
 }
